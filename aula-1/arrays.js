841d1d5fa05b57cb8eea39b17ff24974e3a75773
nicollycff <nicolly.ferreira.francisco@escola.pr.gov.br>
2026-04-30 13:22:56 +0000

    trabalho 1 finalizado

---
+++ aula-1/arrays.js
@@ -0,0 +1,51 @@
+const collyedital = [
+  {
+  titulo: "A biblioteca da meia noite",
+  preco: 15
+  },
+  {
+    titulo: "A sutil arte de ligar o foda-se",
+    preco: 25
+  },
+  {
+    titulo: "A princesa salva a si mesma neste livro",
+    preco: 30
+  },
+  {
+    titulo: "Pensamentos em forma de poesia",
+    preco: 45
+  },
+  {
+    titulo: "A bruxa não vai para a fogueira nesse livro ",
+    preco: 50
+  }
+]
+
+const CFFleitura = [
+  {
+    titulo: "O Sol é para Todos",
+    preco: 20
+  },
+  {
+    titulo: "A Menina que Roubava Livros",
+    preco: 22
+  },
+  {
+    titulo: "A Hipótese do Amor",
+    preco: 28
+  },
+  {
+    titulo: "Apenas Amigos ",
+    preco: 33
+  },
+  {
+    titulo: "É Assim Que Acaba ",
+    preco: 35
+  },
+  {
+    titulo: "A culpa é das estrelas",
+    preco: 40
+  }
+]
+
+module.exports = { collyedital, CFFleitura }
---
+++ aula-1/index.js
@@ -0,0 +1,28 @@
+const {  collyedital, CFFleitura } = require('./arrays');
+
+function juntaListas(lista1, lista2) {
+    let listaFinal = [];
+    let posicaoAtualLista1 = 0;
+    let posicaoAtualLista2 = 0;
+    let atual = 0;
+
+    while (posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length) {
+        let produtoAtualLista1 = lista1[posicaoAtualLista1];
+        let produtoAtualLista2 = lista2[posicaoAtualLista2];
+        console.log('comparando')
+
+        if(produtoAtualLista1.preco < produtoAtualLista2.preco) {
+            listaFinal[atual] = produtoAtualLista1;
+            posicaoAtualLista1++;
+        } else {  
+            listaFinal[atual] = produtoAtualLista2;
+            posicaoAtualLista2++;
+        }
+
+        atual++;
+    }
+    
+    return listaFinal; 
+}
+
+console.log(juntaListas( collyedital, CFFleitura))
